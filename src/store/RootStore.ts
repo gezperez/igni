c081bdf0ce6926fb77b2c1b6353fcbdf4a723a90
Ezequiel Perez <ezequielperez@Ezequiels-MBP.fibertel.com.ar>
2021-06-02 22:29:34 -0300

    WIP

--- src/store/RootStore.ts
+++ src/store/RootStore.ts
@@ -1,1 +1,13 @@
-export class RootStore {}
+import { AppStore } from './AppStore'
+import { AuthStore } from './AuthStore'
+
+export class RootStore {
+  appStore
+
+  authStore
+
+  constructor() {
+    this.appStore = new AppStore()
+    this.authStore = new AuthStore()
+  }
+}
